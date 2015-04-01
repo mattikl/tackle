8eb4faa35297288a66129e28879f95aaead1d93d
Matti Korttila <matti.korttila@gmail.com>
2015-04-01 09:56:12 -0700

    implement createdir command

--- tackle/mgmt.py
+++ tackle/mgmt.py
@@ -1,16 +1,45 @@
 import click
+import os
+
 from . import settings
 from .formats import readers, writers
+
+def mkdir(path):
+    try:
+        os.mkdir(path)
+    except OSError, e:
+        raise click.ClickException("Coudn't create directory: %s" % e)
+
+def touch(filename):
+    try:
+        f = open(filename, 'w')
+        f.write('')
+    except Exception, e:
+        raise click.ClickException("Coudn't create file: %s" % e)
 
 @click.group()
 def mgmt():
     """Tackle management commands"""
 
 @mgmt.command()
-@click.option('--path', help='directory path')
+@click.option('--path', type=click.Path(exists=False),
+              default=os.path.join(os.getenv('HOME'), '.tackle'))
 def createdir(path):
     """Create tackle directory"""
-    click.echo('create dir')
+    if os.path.exists(path):
+        click.echo("Path %s already exists." % path)
+    else:
+        mkdir(path)
+        click.echo("Created tackle directory %s" % path)
+
+    pluginpath = os.path.join(path, "plugins")
+    if not os.path.exists(pluginpath):
+        mkdir(pluginpath)
+    initpy = os.path.join(pluginpath, '__init__.py')
+    if not os.path.exists(initpy):
+        touch(os.path.join(initpy))
+    click.echo("You can now place plugins in %s" % pluginpath)
+
 
 @mgmt.command()
 def createplugin():
