e555a6c4ccd678a330af903556275b7f87a5ece3
Matti Korttila <matti.korttila@gmail.com>
2015-04-01 13:19:56 +0200

    implement mgmt command show formatters

--- tackle/mgmt.py
+++ tackle/mgmt.py
@@ -1,5 +1,6 @@
 import click
 from . import settings
+from .formats import readers, writers
 
 @click.group()
 def mgmt():
@@ -16,7 +17,20 @@ def createplugin():
     """Create tackle plugin"""
     click.echo('create plugin')
 
+def firstline(obj):
+    return obj.splitlines()[0] if isinstance(obj, str) and len(obj) else ""
+
 @mgmt.command()
-def show():
-	"""Show plugins available plugins"""
-	
+@click.option('-r', '--readers', 'showread', is_flag=True, help='show only readers')
+@click.option('-w', '--writers', 'showwrite', is_flag=True, help='show only writers')
+def show(showread, showwrite):
+    """Show available formatters"""
+    showall = showread == showwrite
+    if showread or showall:
+        click.echo("Readers:")
+        for r, fun in readers.iteritems():
+            click.echo("\t%s\t\t%s" % (r, firstline(fun.__doc__)))
+    if showwrite or showall:
+        click.echo("Writers:")
+        for r, fun in writers.iteritems():
+            click.echo("\t%s\t\t%s" % (r, fun.__doc__))
