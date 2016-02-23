4e738378e564ffd0e753f40a727c656072ad274f
Matti Korttila <matti.korttila@gmail.com>
2016-02-23 13:54:09 +0200

    make namedtuple field names alphanumeric
    Non-alphanumeric characters in field names resulted
    in exceptions, so now they are converted to '_'.
    
    namedtuples are good for reducing memory usage
    compared to dictionaries. If preserving original
    field names is essential, then they should be
    stored separately and added to the result.

--- tackle/utils.py
+++ tackle/utils.py
@@ -1,5 +1,8 @@
 from collections import namedtuple
+import re
 
-def makerow(columns):
-    return namedtuple('Row', columns)
+def make_alphanumeric(value):
+    return re.sub(r'\W', '_', value)
 
+def makerow(headers):
+    return namedtuple('Row', map(make_alphanumeric, headers))
